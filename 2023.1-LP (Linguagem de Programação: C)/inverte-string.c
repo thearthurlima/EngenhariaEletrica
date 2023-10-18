ea7084704c203da16ba9e5a1227f621329e3db0d
Arthur Lima <115672061+thearthurlima@users.noreply.github.com>
2023-10-18 15:22:34 -0300

    alterações no código

--- 2023.1-LP (Linguagem de Programação: C)/inverte-string.c
+++ 2023.1-LP (Linguagem de Programação: C)/inverte-string.c
@@ -1,4 +1,4 @@
-//12) Inverte uma matriz dada pelo usuário 
+//Inverte uma matriz fornecida pelo usuário.
 
  #include <stdio.h> 
  #include <stdlib.h> 
@@ -8,32 +8,32 @@
   
  int main() 
  { 
-         char string[MAX], stringaux[MAX]; 
-         int tamanhoDaString = 0, k = 0; 
+        char string[MAX], stringaux[MAX]; 
+        int tamanhoDaString = 0, i = 0, j = 0, k = 0; 
 
-         printf("Digite uma string: ");
-         fgets(string, MAX, stdin); 
+        printf("Digite uma string: ");
+        fgets(string, MAX, stdin); 
   
-         while(string[k] != '\0') //Pega o tmanho da string
-         { 
-                 tamanhoDaString++; 
-                 k++; 
-         } 
-         tamanhoDaString--; 
+  		//Pega o tmanho da string
+        while(string[k] != '\0') 
+        { 
+                tamanhoDaString++; 
+                k++; 
+        } 
+        tamanhoDaString--; 
 
-         for(int i = 0, j = tamanhoDaString-1; i < tamanhoDaString; i++, j--) //Inverte a string principal na string auxiliar
-         { 
-                 stringaux[i] = string[j]; 
-         } 
+		//Inverte a string principal na string auxiliar
+        for(i = 0, j = tamanhoDaString-1; i < tamanhoDaString; i++, j--)
+        { 
+                stringaux[i] = string[j]; 
+        } 
   
-         for(int i = 0; string[i] != '\0'; i++) //Copia a string invertida na string principal
-         { 
-                 string[i] = stringaux[i]; 
-         } 
-  
-         printf("String invertida: ");
-         for(int i = 0; string[i] != '\0'; i++) //Exibe a string
-         { 
-                 printf("%c", string[i]); 
-         }   
+		//Copia a string invertida na string principal
+        for(i = 0; string[i] != '\0'; i++) 
+        { 
+                string[i] = stringaux[i]; 
+        } 
+        
+        //Exibe a string invertida
+        printf("String invertida: %s", string);
  }
