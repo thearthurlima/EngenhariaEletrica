1b1cc0d2827d449596aecfdd57dec994f7cd8591
Arthur Lima <115672061+thearthurlima@users.noreply.github.com>
2023-10-18 16:17:02 -0300

    identation changes to inverte-string.c

--- 2023.1-LP (Linguagem de Programação: C)/inverte-string.c
+++ 2023.1-LP (Linguagem de Programação: C)/inverte-string.c
@@ -1,4 +1,4 @@
-//Inverte uma matriz fornecida pelo usuário.
+//Inverte uma matriz fornecida pelo usuÃ¡rio.
 
  #include <stdio.h> 
  #include <stdlib.h> 
@@ -8,28 +8,28 @@
   
  int main() 
  { 
-        char string[MAX], stringaux[MAX], aux; 
-        int tamanhoDaString = 0, i = 0, j = 0, k = 0; 
+	char string[MAX], stringaux[MAX], aux; 
+	int tamanhoDaString = 0, i = 0, j = 0, k = 0; 
 
-        printf("Digite uma string: ");
-        fgets(string, MAX, stdin); 
+	printf("Digite uma string: ");
+	fgets(string, MAX, stdin); 
   
-		//Pega o tmanho da string sem contar o '\n'
-        while(string[k] != '\n') { 
-                tamanhoDaString++; 
-                k++; 
-        }
+	//Pega o tmanho da string sem contar o '\n'
+	while(string[k] != '\n') { 
+        tamanhoDaString++; 
+        k++; 
+	}
         
-		//Inverte a string principal na string auxiliar
-        for(i = 0, j = tamanhoDaString-1; i < tamanhoDaString; i++, j--) { 
-                stringaux[i] = string[j]; 
-        } 
+	//Inverte a string principal na string auxiliar
+	for(i = 0, j = tamanhoDaString-1; i < tamanhoDaString; i++, j--) { 
+        stringaux[i] = string[j]; 
+	} 
 
-		//Copia a string invertida na string principal
-        for(i = 0; string[i] != '\0'; i++) { 
-                string[i] = stringaux[i]; 
-        }
+	//Copia a string invertida na string principal
+	for(i = 0; string[i] != '\0'; i++) { 
+        string[i] = stringaux[i]; 
+	}
         
-        //Exibe a string invertida
-        printf("String invertida: %s", string);
+	//Exibe a string invertida
+	printf("String invertida: %s", string);
  }
