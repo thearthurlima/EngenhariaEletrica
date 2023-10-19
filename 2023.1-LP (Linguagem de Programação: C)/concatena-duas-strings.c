75db46ce9b3976c22dacf2d4b99fd59a239de708
Arthur Lima <115672061+thearthurlima@users.noreply.github.com>
2023-10-18 21:40:29 -0300

    Create concatena-duas-strings.c

---
+++ 2023.1-LP (Linguagem de Programação: C)/concatena-duas-strings.c
@@ -0,0 +1,28 @@
+// Concatenar duas strings.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 50
+
+int main() {
+	char string1[MAX], string2[MAX], concatString[2*MAX];
+	int i, j;
+	
+	printf("Digite a primeira string:\n");
+	fgets(string1, MAX, stdin);
+	printf("\nDigite a segunda string:\n");
+	fgets(string2, MAX, stdin);
+	
+	for(i = 0; string1[i] != '\0'; i++) { //Coloca a string1 em concatstring
+		concatString[i] = string1[i];
+	}
+	
+	for(i = strlen(string1)-1, j = 0; string2[j] != '\0'; i++, j++) { //Coloca a string2 em concatstring
+		concatString[i] = string2[j];
+		concatString[i+1] = '\0';
+	}
+	
+	printf("\nString concatenada:\n%s", concatString);
+}
