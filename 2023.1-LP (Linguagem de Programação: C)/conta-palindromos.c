fff53332fa937f47fa73b8ba96789ddae1f01cc8
Arthur Lima <115672061+thearthurlima@users.noreply.github.com>
2023-10-25 15:29:03 -0300

    Update and rename achar palindromos e contar quantos.c to conta-palindromos.c

---
+++ 2023.1-LP (Linguagem de Programação: C)/conta-palindromos.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+int main() {
+    char string[MAX];
+    int i, j, k, primLetra, ultLetra, tamPalavra, flag, contagem = 0;
+
+    printf("Digite uma string: ");
+	fgets(string, MAX, stdin);
+
+    for (i = 0; string[i] != '\0'; i++) {
+        if (string[i] != ' ' && string[i] != '\n') {
+            primLetra = i;
+            for (j = i; string[j] != ' ' && string[j] != '\n' && string[j] != '\0'; j++) {
+                ultLetra = j;
+            }
+
+            tamPalavra = ultLetra - primLetra + 1;
+
+            flag = 1; // Coloca flag em 1: Assume que a palavra eh palindromo
+            for (k = 0; k < tamPalavra / 2; k++) {
+                if (string[primLetra + k] != string[ultLetra - k]) {
+                    flag = 0; //Coloca flag em 0 se nao for palindromo
+                    break;
+                }
+            }
+            if (flag == 1) {
+                contagem++;
+            }
+            
+            i = ultLetra;
+        }
+    }
+
+    printf("\nContagem de palindromos: %d", contagem);
+
+    return 0;
+}
